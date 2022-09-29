872f1100513d84ee6cf83e8edb9855dbe16c22b3
Yu Tanaka <githubam1@am1.jp>
2022-09-29 16:22:10 +0900

    フェーズ切り替えテスト通過

--- Assets/AM1PhaseSystem/Tests/Runtime/PhaseChangeTests.cs
+++ Assets/AM1PhaseSystem/Tests/Runtime/PhaseChangeTests.cs
@@ -23,7 +23,7 @@ public class PhaseChangeTests
 
         Assert.That(phaseManager.ChangeRequest(bench), Is.True, "切り替え要求");
         yield return null;
-        yield return null;
+        yield return new WaitForFixedUpdate();
         Assert.That(bench.initCount, Is.EqualTo(1), $"初期化を1回実行");
         Assert.That(bench.updateCount, Is.GreaterThan(0), $"Updateを{bench.updateCount}回実行");
         Assert.That(bench.fixedUpdateCount, Is.GreaterThan(0), $"FixedUpdateを{bench.fixedUpdateCount}回実行");
@@ -34,14 +34,14 @@ public class PhaseChangeTests
         bench.canChange = true;
         Assert.That(phaseManager.ChangeRequest(bench2), Is.True, "bench2へ切り替え登録");
         yield return null;
-        yield return null;
+        yield return new WaitForFixedUpdate();
         Assert.That(bench2.initCount, Is.EqualTo(0), "前の処理を終了していないので初期化はまだ");
         // 終了
         bench.isTerminated = true;
         yield return null;
         Assert.That(bench2.initCount, Is.EqualTo(1), $"初期化を1回実行");
         Assert.That(bench2.updateCount, Is.GreaterThan(0), $"Updateを{bench2.updateCount}回実行");
-        yield return null;
+        yield return new WaitForFixedUpdate();
         Assert.That(bench2.fixedUpdateCount, Is.GreaterThan(0), $"FixedUpdateを{bench2.fixedUpdateCount}回実行");
         Assert.That(bench2.terminateCount, Is.EqualTo(0), $"Terminateはまだ呼ばれていない。");
 
