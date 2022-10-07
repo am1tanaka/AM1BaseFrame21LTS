23deed96739b793b418c91b72017b47b2ca18ff6
Yu Tanaka <githubam1@am1.jp>
2022-10-07 22:31:07 +0900

    Stateのサンプル

--- Assets/AM1State/Samples~/Sample/TestStateStack.cs
+++ Assets/AM1State/Samples~/Sample/TestStateStack.cs
@@ -17,12 +17,67 @@ public class TestStateStack : AM1StateStack
         states[2].SetNext(null);
     }
 
+
+
     protected override void Update()
     {
         base.Update();
 
         if ((CurrentState == null) && Input.GetKeyDown(KeyCode.RightArrow)) {
             this.PushRequest(states[0]);
+        }
+
+        // 1,2,3キー - 現在の状態から押したキーの状態に切り替え
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PopAndPushRequest(states[0]);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PopAndPushRequest(states[1]);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            PopAndPushRequest(states[2]);
+        }
+
+        // Q,W,Eキー - 現在の状態を中断して、対応する状態(Q = TestState1, W = TestState2, E = TestState3)に切り替える
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            PushRequest(states[0]);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            PushRequest(states[1]);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            PushRequest(states[2]);
+        }
+
+        // A,S,Dキー - 押したキーがスタックにあったら、その状態(A = TestState1, S = TestState2, D = TestState3)まで戻す
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PopRequest(states[0]);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            PopRequest(states[1]);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            PopRequest(states[2]);
+        }
+
+        // Rキー - 最初の状態まで戻します
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PopToRootRequest();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            // 0キー - 全ての状態を戻して状態なしにします
+            PopAllRequest();
         }
     }
 
